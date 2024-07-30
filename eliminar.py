39702d24b4dd9dd375f6573e5899b5c063be5a51
Santiago-SanP05 <santiagopinzoncruz@gmail.com>
2024-07-30 12:53:03 -0500

    fix:(se arregló y mejoró la funcion de eliminar):Bug:

--- eliminar.py
+++ eliminar.py
@@ -3,9 +3,12 @@ from datos import *
 def elim():
     Json = cargar_datos()
     while True:
-        doc = str(input("----escribe porfavor tu documento para eliminar----"))
+        print("----escribe porfavor tu documento para eliminar----")
+        doc = input("->")
         if str(doc) in Json["Usuarios"]:
             Json["Usuarios"].pop(doc)
+            guardar_datos(Json)
+            break
         else:
             print("***********************************************************")
             print("______Error este doc no existe______")
